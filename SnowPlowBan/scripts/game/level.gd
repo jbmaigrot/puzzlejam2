447ff9a7a1a10908f1346769b3ed45f829c8d899
Damien D <kerdelos@gmail.com>
2020-06-15 22:28:59 +0200

    generating terrain based on the level

--- SnowPlowBan/scripts/game/level.gd
+++ SnowPlowBan/scripts/game/level.gd
@@ -3,11 +3,16 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		
-	for x in range (0, 5):
-		for z in range (0,5):
+	var level_state = Globals.psengine.get_level_state();
+	
+	for x in range (0, level_state.width):
+		for z in range (0,level_state.height):
 			$GridMap.set_cell_item(x,0,z,0);
+			for y in range (0, level_state.height-z):
+				if y == level_state.height-z-1:
+					$GridMap.set_cell_item(x,y+1,z,1,7);
+				else :
+					$GridMap.set_cell_item(x,y+1,z,0);
 			
-	for x in range (0, 5):
-			$GridMap.set_cell_item(x,1,0,1,x);
+	
 
