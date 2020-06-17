2b3b1ad1da8e4497995977c98e02d348cfba1c49
Damien D <kerdelos@gmail.com>
2020-06-17 15:55:22 +0200

    some graphic tweaks

--- SnowPlowBan/scripts/game/level.gd
+++ SnowPlowBan/scripts/game/level.gd
@@ -29,12 +29,12 @@ func _ready():
 	
 	for x in range (0, level_state.width):
 		for z in range (0,level_state.height):
-			$GridMap.set_cell_item(x,0,z,0);
+			$GridMap.set_cell_item(x,0,z,2);
 			for y in range (0, level_state.height-z):
 				if y == level_state.height-z-1:
 					$GridMap.set_cell_item(x,y+1,z,1,16);
 				else :
-					$GridMap.set_cell_item(x,y+1,z,0);
+					$GridMap.set_cell_item(x,y+1,z,2);
 	
 	generate_dynamic_objects();
 	
